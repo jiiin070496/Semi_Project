31164a8d8e48ff728f6e4001a872039e88a90912
jiiin070496 <jin03154@naver.com>
2023-08-02 15:23:45 +0900

    EncodingFilter

---
+++ src/main/java/semi_project/common/filter/EncodingFilter.java
@@ -0,0 +1,26 @@
+package semi_project.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("[냄궁필터] filter 진짜 들어옴?");
+		// 한글 깨짐 방지
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8"); 
+		response.setContentType("text/html; charset=UTF-8");
+		
+		 //다음 filter 동작 또는 doGet()/ doPost() 호출
+		chain.doFilter(request, response);
+	}
+
+}
